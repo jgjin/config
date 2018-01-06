d2f52786dc41aa86bfd0368682f4fb1616241dfa
jason-banana <jgjin@umich.edu>
2018-01-06 00:42:51 -0500

    Added behavior to Firefox, added LeechBlock add-on to Firefox, and corrected packages

--- bin/firefox-private.sh
+++ bin/firefox-private.sh
@@ -3,6 +3,10 @@
 if [[ "$#" -eq 0 ]]; then
     bspc rule -a "Firefox Beta" --one-shot desktop=4
 else
-    bspc rule -a "Firefox Beta" --one-shot desktop=$1
+    if [[ "$1" -eq 0 ]]; then
+	bspc rule -a "Firefox Beta" --one-shot hidden=on desktop=$1
+    else
+	bspc rule -a "Firefox Beta" --one-shot desktop=$1
+    fi
 fi
 firefox-beta --private-window
